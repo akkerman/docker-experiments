19ca318f089bd961c32bc74ef3c1f7a894b5e69c
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2017-10-01 20:58:44 +0200

    add app to a sub-context

--- wsgi/python1/app.py
+++ wsgi/python1/app.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/hello')
 def hello_world():
@@ -8,3 +10,20 @@ def hello_world():
 @app.route('/hi')
 def hi_world():
     return 'p1: Hi, World!'
+
+class CtxRootMiddleWare(object):
+    def __init__(self, app, ctx_root=''):
+        self.app = app
+        self.ctx_root = ctx_root
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.ctx_root):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.ctx_root):]
+            environ['SCRIPT_NAME'] = self.ctx_root
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url is not part of the app.".encode()]
+
+app.wsgi_app = CtxRootMiddleWare(app.wsgi_app, '/p1')
