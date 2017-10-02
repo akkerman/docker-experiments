8790cea9df6723d10a1e1a11cfb917bec1653e4b
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2017-10-02 11:01:42 +0200

    remove custom handler of context root
    use uwsgi mount point

--- wsgi/python1/app.py
+++ wsgi/python1/app.py
@@ -11,24 +11,3 @@ def hello_world():
 def hi_world():
     return 'p1: Hi, World!'
 
-class CtxRootMiddleWare(object):
-    def __init__(self, app, ctx_root=''):
-        self.app = app
-        self.ctx_root = ctx_root
-
-    def __call__(self, environ, start_response):
-        ctx_root = self.ctx_root
-
-        if 'CTX_ROOT' in environ:
-            ctx_root = environ['CTX_ROOT']
-
-        if environ['PATH_INFO'].startswith(ctx_root):
-            environ['PATH_INFO'] = environ['PATH_INFO'][len(ctx_root):]
-            environ['SCRIPT_NAME'] = ctx_root
-            return self.app(environ, start_response)
-        else:
-            start_response('404', [('Content-Type', 'text/plain')])
-            return ["This url is not part of the app.".encode()]
-
-
-app.wsgi_app = CtxRootMiddleWare(app.wsgi_app)
