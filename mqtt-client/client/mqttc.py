9a533a87168edef582ae5e5c6d79a9f224cefeb6
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2017-10-07 16:19:36 +0200

    client for ever listening, but exit on signal

--- mqtt-client/client/mqttc.py
+++ mqtt-client/client/mqttc.py
@@ -1,25 +1,50 @@
+import signal
 import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+
+def disconnect(signum, frame):
+    client.publish('status/mqttc', 'offline', qos=0, retain=True)
+    client.loop()
+    client.disconnect()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print('Connected with result code '+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("development/#")
+    client.publish('status/mqttc', 'online', qos=0, retain=True)
+    client.subscribe([('development/#', 1), ('research/#', 1)])
 
-# The callback for when a PUBLISH message is received from the server.
+def on_disconnect(client, userdat, rc):
+    if rc == 0:
+        print("disconnected")
+        sys.exit(0)
+    print("unexpected disconnect")
+
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+def on_message_development(client, userdata, msg):
+    print("processing development")
 
-client.connect("broker", 1883, 60)
+def on_message_research(client, userdata, msg):
+    print("processing research")
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
+
+def setup():
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+    client.will_set('status/mqttc', payload="disconnected", qos=1, retain=True)
+    client.message_callback_add("development/#", on_message_development)
+    client.message_callback_add("research/#", on_message_research)
+    print('connecting')
+    client.connect('broker', 1883, 60)
+    signal.signal(signal.SIGINT, disconnect)
+    signal.signal(signal.SIGTERM, disconnect)
+
+
+setup()
 client.loop_forever()
